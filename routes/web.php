1db48d5636883e4535763fe1c500528e66f55421
SameerAyoub <sameerayoub34@yahoo.com>
2018-10-04 16:33:03 +0500

    front end 1

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,29 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.blog');
 });
+
+Route::get('/post', function () {
+    return view('user.post');
+})->name('post');
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.home');
+})->name('admin.home');
+
+Route::get('/admin/post', function () {
+    return view('admin.post.post');
+})->name('admin.post');
+
+Route::get('/admin/tag', function () {
+    return view('admin.tag.tag');
+})->name('admin.tag');
+
+Route::get('/admin/category', function () {
+    return view('admin.category.category');
+})->name('admin.category');
+
+
